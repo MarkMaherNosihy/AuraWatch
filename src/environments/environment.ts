393f2247b5594ebda4a3ddefe9ce84340c045905
MarkMaher <markdevs159@gmail.com>
2024-09-17 17:25:27 +0300

    Changed API Base

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
     token: 'eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI5MGFiMTIxM2ZiMjQzOTgzMzQyNTllZGE1NmU5MzhjZCIsIm5iZiI6MTcyNDYyNzY2NS4wODYwMTcsInN1YiI6IjY2Y2JiOWY4NjgxZTUwNmQ3MTIwNTkwYSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.A_UgPqUWHt-4O666G4EooXQ6vcpqC2HCKtWfE25CqtI',
     apiKey: '90ab1213fb24398334259eda56e938cd',
-    base: 'https://aura-watch-back-end.vercel.app',
+    base: 'https://aurawatchbackend-production.up.railway.app',
     publicBase: 'https://api.themoviedb.org/3'
 };
